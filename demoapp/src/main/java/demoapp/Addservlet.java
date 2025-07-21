7094395c4a4bda68948a66c4a07e92511bb702d8
VANGAPRIYANKA625 <230101120001@centurionuniv.edu.in>
2025-07-21 20:41:26 +0530

    SERVLET

---
+++ demoapp/src/main/java/demoapp/Addservlet.java
@@ -0,0 +1,28 @@
+package demoapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Addservlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		k=k*k;
+        req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+}
+}
+
---
+++ demoapp/src/main/java/demoapp/SqServlet.java
@@ -0,0 +1,20 @@
+package demoapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int k = (int) req.getAttribute("k");
+		PrintWriter out = res.getWriter();
+		out.println("Hello to square");
+		out.println("Result is: " + k);
+
+}
+}
